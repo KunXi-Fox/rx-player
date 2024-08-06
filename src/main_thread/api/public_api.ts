4b93c070764c90f5f53874e1bc3c09a95af9fad4
KunXi-Fox <Kun.Xi@foxtel.com.au>
2024-08-06 16:55:50 +0800

    VPW-1700 | Fix Tizen 2017 not works

--- src/main_thread/api/public_api.ts
+++ src/main_thread/api/public_api.ts
@@ -348,7 +348,7 @@ class Player extends EventEmitter<IPublicAPIEvent> {
     addFeatures(featureList);
   }
 
-  static Features: IFeature[] = [];
+  static Features: Record<string, IFeature> = {};
 
   /**
    * Register the video element to the set of elements currently in use.
